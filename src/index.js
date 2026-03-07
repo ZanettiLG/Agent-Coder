086b2e6645855083d1e8a5e06ac47d6242141f4b
Zanetti <luis.zanetti@ip.tv>
2026-03-06 22:42:48 -0300

    Update index.js to include workspace parameter in coder function call for enhanced command execution context.

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 const coder = require("./coder");
 
 const main = async () => {
-    const response = await coder("/ask how is this repository actual scenario? write SPEC");
+    const response = await coder("/ask how is this repository actual scenario? write SPEC", {workspace: "../test"});
     console.log(response);
 }
 
